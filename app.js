e35e4ee8408181c7f0b1c37012541d5fcdb6d6fc
Alaricode <antonlarichev@gmail.com>
2022-12-02 17:24:51 +0300

    Sets

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('Hello')
+const flights = ['Russia', 'USA', 'London',  'London', 'USA'];
+
+const setFlights = new Set(flights);
+console.log(setFlights);
+console.log(setFlights.size);
+console.log(setFlights.has('Russia'));
+setFlights.add('Paris');
+setFlights.delete('London');
+console.log(setFlights);
+
+for (const flight of setFlights) {
+	console.log(flight);
+}
+
+console.log([...setFlights]);
+
+const setObj = new Set([{ a: 1 }, { b: 2 }, { b: 2 }]);
+console.log(setObj);
+console.log(new Set('abcd'));
