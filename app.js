fe845cce8dadccfd981c0869e8bdcad9db5c0334
Alaricode <antonlarichev@gmail.com>
2022-12-09 15:39:18 +0300

    Быстрое создание Map

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 'use strict';
-const wetherMap = new Map();
-wetherMap
-	.set('London', '10')
-	.set('Moscow', '7');
 
-console.log(wetherMap.get('Moscow'));
-console.log(wetherMap.get('not found'));
-
-console.log(wetherMap.has('Moscow'));
-console.log(wetherMap.has('not found'));
-console.log(wetherMap.delete('London'));
-
-
-const arr = [1, 2, 3];
-wetherMap
-	.set(1, 5)
-	.set(true, 'yes')
-	.set(false, 'no')
-	.set(arr, 'array')
-	.set({ a : 1 },  { b: 1 });
-
+const wetherMap = new Map([
+	['London', '10'],
+	['Moscow', '7'],
+]);
 console.log(wetherMap);
-console.log(wetherMap.size);
 
-console.log(wetherMap.get(arr));
-console.log(wetherMap.get({ a : 1 }));
+const wetherObject = {
+	london: 10,
+	moscow: 7,
+	paris: 14
+};
+
+console.log(Object.entries(wetherObject));
+const wetherMap2 = new Map(Object.entries(wetherObject));
+console.log(wetherMap2.size)
