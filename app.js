3dc17f32603bd4c2d057bf4b219616c8c7f5d118
Alaricode <antonlarichev@gmail.com>
2022-12-09 17:21:58 +0300

    WeakMap

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 'use strict';
-/* Необходимо поменять местами ключи и значения в следующем Map */
-let weatherMap = new Map([
-	['London', 10],
-	['Moscow', 7],
-	['Paris', 14],
-]);
+let a = { a: 1 };
+let b = { b: 1 };
+const map = new WeakMap();
+map.set(a, 'testA');
+map.set(b, 'testB');
+console.log(map.get(a));
+console.log(map.has(a));
+console.log(map);
 
-weatherMap = new Map([...weatherMap].map(el => el.reverse()));
-console.log(weatherMap);
+a = null;
+setTimeout(() => {
+	console.log(map);
+}, 1000);
+
+let cache = new WeakMap();
+
+function getValue(obj) {
+	if (!cache.has(obj)) {
+		const res = 1; /* сложные вычиления ))) */
+		cache.set(obj, res);
+	}
+	return cache.get(obj);
+}
+
+const res = getValue(b);
+console.log(res);
+const res2 = getValue(b);
+console.log(res2);
