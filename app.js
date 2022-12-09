c3e67441701e042203f99c7732eb6eae8519ad6d
Alaricode <antonlarichev@gmail.com>
2022-12-09 17:30:12 +0300

    WeakSet

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 'use strict';
-let a = { a: 1 };
-let b = { b: 1 };
-const map = new WeakMap();
-map.set(a, 'testA');
-map.set(b, 'testB');
-console.log(map.get(a));
-console.log(map.has(a));
-console.log(map);
 
+let a = { a: 1 };
+let b = { b: 2 };
+const set = new WeakSet([a, b]);
 a = null;
 setTimeout(() => {
-	console.log(map);
+	console.log(set);
 }, 1000);
-
-let cache = new WeakMap();
-
-function getValue(obj) {
-	if (!cache.has(obj)) {
-		const res = 1; /* сложные вычиления ))) */
-		cache.set(obj, res);
-	}
-	return cache.get(obj);
-}
-
-const res = getValue(b);
-console.log(res);
-const res2 = getValue(b);
-console.log(res2);
