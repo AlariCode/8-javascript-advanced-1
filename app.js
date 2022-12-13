418215d003c31b9d5a1b04524879bf876f8c47f5
Alaricode <antonlarichev@gmail.com>
2022-12-13 16:43:53 +0300

    Оператор остатка от деления

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
-/*
-	Написать функцию, которая принимает min и max
-	и возвращает случайное целое число между ними, включая их
-*/
 
-function random(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+console.log(15 / 2);
+console.log(15 % 2); // 14 / 2 + 1 = 15
+console.log(14 % 2); // 14 / 2 = 7
+
+const isEven = n => n % 2 === 0;
+const isOdd = n => n % 2 === 1;
+function isEvenFunc(n) {
+	return n % 2 === 0;
 }
-console.log(random(1, 20));
+
+console.log(isEven(17));
+console.log(isEven(12));
+console.log(isOdd(17));
+console.log(isOdd(12));
