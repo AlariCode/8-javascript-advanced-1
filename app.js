8343e3693798ef7d929f51512d6b85b144533e76
Alaricode <antonlarichev@gmail.com>
2022-12-13 14:48:42 +0300

    Упражнение - Функция случайного числа

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 'use strict';
+/*
+	Написать функцию, которая принимает min и max
+	и возвращает случайное целое число между ними, включая их
+*/
 
-console.log(Math.round(1.4));
-console.log(Math.round(1.6));
-console.log(Math.round(1.499999));
-
-console.log(Math.ceil(1.1));
-console.log(Math.ceil(1));
-console.log(Math.ceil(0.1));
-
-console.log(Math.floor(0.1));
-console.log(Math.floor(2.9));
-
-console.log(Math.trunc(2.9));
-console.log(Math.trunc(2.1));
-console.log(Math.trunc(1.1111));
-
-
-console.log(1.499999.toFixed(1));
-console.log(Number((1.499999).toFixed(1)));
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(random(1, 20));
