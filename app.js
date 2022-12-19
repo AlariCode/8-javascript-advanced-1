c688cb93628231a51f49b6654a07563c48c41403
Alaricode <antonlarichev@gmail.com>
2022-12-19 18:43:14 +0300

    Работа с BigInt

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 'use strict';
 
-const bigNum1 = 350_500_000;
-const bigNum2 = 350500000;
+const max = 2**53 -1
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(max);
+console.log(max + 1);
+console.log(max + 2);
+console.log(max + 3);
 
-const payment = 20_10;
-const paymentInR = 20.10;
-const paymentInR2 = 2_0.1_0;
+console.log(2342342343524623465745345345n);
+console.log(BigInt(2342342343524623465745345345));
+console.log(BigInt('2342342343524623465745345345'));
 
-console.log(Number('350_500_000'));
-console.log(Number('350500000'));
-console.log(Number.parseInt('350_500_000'));
-console.log(Number.parseFloat('350_500_000.10'));
+console.log(10n + 10n);
+console.log(10n + BigInt(10));
+console.log(10n * 10n);
+console.log(2342342343524623465745345345n * 2342342343524623465745345345n);
+
+console.log(10n * BigInt(10));
+console.log(10n / 3n);
+console.log(10 / 3);
+
+
+console.log(10n < 20);
+console.log(10n == 10);
+console.log(10n === 10);
+console.log(typeof 10n);
