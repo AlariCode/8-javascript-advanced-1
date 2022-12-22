49ac16bbced5ae075c23f5148d0ba10eca5671db
Alaricode <antonlarichev@gmail.com>
2022-12-22 20:32:38 +0300

    Упражнение - Конвертер валют

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 'use strict';
+/*
+	Напишите функцию, которая принимает 3 параметра:
+	- Сумма
+	- Валюта исходная
+	- Валюта для конвертации
+	И возвращает строку уже сконвертированной суммы с постфиксом
+	валюты. Если не смог, то null.
+	Для примера 3 валюты.
+*/
 
-const options1 = {
-	style: 'currency',
-	currency: 'RUB'
+function convert(sum, initialCurrency, convertCurrency) {
+	const allCurrencies = [
+		{ name: 'USD', mult: 1 },
+		{ name: 'RUB', mult: 1/60 },
+		{ name: 'EUR', mult: 1.1 },
+	];
+	const initial = allCurrencies.find(c => c.name === initialCurrency);
+	if (!initial) {
+		return null;
+	}
+	const convert = allCurrencies.find(c => c.name === convertCurrency);
+	if (!convert) {
+		return null;
+	}
+	return new Intl
+		.NumberFormat('ru-RU', { style: 'currency', currency: convert.name })
+		.format(sum * initial.mult / convert.mult);
 }
 
-const options2 = {
-	style: 'currency',
-	currency: 'USD'
-}
-
-const options3 = {
-	style: 'decimal',
-}
-
-const options4 = {
-	style: 'percent',
-}
-
-const options5 = {
-	style: 'unit',
-	unit: 'celsius',
-}
-
-console.log(new Intl.NumberFormat('ru-RU', options1).format(23000));
-console.log(new Intl.NumberFormat('en-US', options2).format(23000));
-console.log(new Intl.NumberFormat('ru-RU', options3).format(10000));
-console.log(new Intl.NumberFormat('ru-RU', options4).format(0.1));
-console.log(new Intl.NumberFormat('ru-RU', options5).format(25));
+console.log(convert(10000, 'RUB', 'USD'));
+console.log(convert(10000, 'RUB', 'EUR'));
+console.log(convert(100, 'USD', 'RUB'));
+console.log(convert(100, 'USD', 'EUR'));
+console.log(convert(100, 'EUR', 'RUB'));
+console.log(convert(100, 'TG', 'RUB'));
+console.log(convert(100, 'EUR', 'TG'));
