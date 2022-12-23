f5fc5bd8ded2e097429db1e72802f37662f2d96e
Alaricode <antonlarichev@gmail.com>
2022-12-23 19:33:38 +0300

    Точность таймеров

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 'use strict';
 
-const date = new Date();
+const mark1 = performance.now();
 
-console.log(date);
-console.log(new Intl.DateTimeFormat('ru-RU').format(date));
-const options1 = {
-	hour: 'numeric',
-	minute: 'numeric'
-}
-console.log(new Intl.DateTimeFormat('ru-RU', options1).format(date));
-const options2 = {
-	hour: 'numeric',
-	minute: 'numeric',
-	month: 'long',
-	weekday: 'short',
-	year: '2-digit'
-}
-console.log(new Intl.DateTimeFormat('en-US', options2).format(date));
-console.log(new Intl.DateTimeFormat('ru-RU', options2).format(date));
-
-console.log(navigator.language);
-console.log(new Intl.DateTimeFormat(navigator.language, options1).format(date));
+setTimeout(() => {
+	const mark2 = performance.now();
+	console.log(mark2 - mark1);
+}, 1000);
