42a2a5af465f20738cb5634c1a56250918c9058b
Alaricode <antonlarichev@gmail.com>
2022-12-23 17:46:39 +0300

    Сравнение дат

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const date1 = new Date(2024, 10, 15);
-const date2 = new Date(2024, 11, 15);
-console.log(Number(date1));
-console.log(date2 - date1);
+const first = new Date(2024, 10, 4);
+const second = new Date(2024, 10, 4);
 
-function getDaysBetweenDates(dateFirst, dateSecond) {
-	return (date2 - date1) / (1000 * 60 * 60 * 24);
-}
+console.log(first < second);
+console.log(first == second);
+console.log(first === second);
 
-console.log(getDaysBetweenDates(date1, date2));
+console.log(first.getTime() == second.getTime());
+console.log(first.getTime() === second.getTime());
+console.log(Number(first) === Number(second));
+console.log(+first === +second);
