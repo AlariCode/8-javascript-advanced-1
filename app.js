cddab0416b58b4a394fd02ba0fdd8147f8152485
Alaricode <antonlarichev@gmail.com>
2022-12-23 17:39:36 +0300

    Операции с датами

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 'use strict';
 
-const now = new Date();
-console.log(now);
+const date1 = new Date(2024, 10, 15);
+const date2 = new Date(2024, 11, 15);
+console.log(Number(date1));
+console.log(date2 - date1);
 
-console.log(new Date('02-01-2023'));
-console.log(new Date('02/01/2023'));
-console.log(new Date('2023/01/02'));
-console.log(new Date('10 Jan 2023'));
-console.log(new Date('10 янв 2023'));
-console.log(new Date('Dec 22 2022 20:40:03'));
+function getDaysBetweenDates(dateFirst, dateSecond) {
+	return (date2 - date1) / (1000 * 60 * 60 * 24);
+}
 
-console.log(new Date(2024, 11, 31, 10, 5, 10));
-console.log(new Date(2024, 12, 10 + 100));
-console.log(new Date(0));
-console.log(new Date(1 * 24 * 60 * 60 * 1000));
-console.log(Date.now());
-console.log(new Date(Date.now()));
-
-console.log(now.getFullYear());
-console.log(now.getMonth());
-console.log(now.getDate());
-console.log(now.getDay());
-console.log(now.getHours());
-console.log(now.getMinutes());
-console.log(now.getTime());
-
-console.log(new Date(now.setFullYear(2030)));
-console.log(new Date(now.setMonth(10)));
+console.log(getDaysBetweenDates(date1, date2));
