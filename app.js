8b419b4a256c98928478408d01106d0c6a96e620
Alaricode <antonlarichev@gmail.com>
2023-01-13 19:57:53 +0300

    Классы

--- app.js
+++ app.js
@@ -3,11 +3,27 @@
 const Book = function(title, author) {
 	this.author = author;
 	this.title = title;
+	this.isRead = false;
 }
-Book.prototype.isRead = false;
+Book.prototype.read = function() {
+	this.isRead = true;
+};
 
-const lordOfTheRings = new Book('1', '1');
+class BookClass {
+	isRead = false;
 
-console.log(lordOfTheRings.hasOwnProperty('title'));
+	constructor(title, author) {
+		this.author = author;
+		this.title = title;
+	}
 
-console.log(Book.prototype.__proto__);
+	read() {
+		this.isRead = true;
+	}
+}
+
+const lotr = new BookClass('lotr', 'Tolkien');
+console.log(lotr);
+console.log(lotr instanceof BookClass);
+lotr.read();
+console.log(lotr.__proto__);
