3dbf6ec7dbb5667b47ba79cedb8913c20d7530ab
Alaricode <antonlarichev@gmail.com>
2023-01-13 16:59:37 +0300

    Оператор new

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 'use strict';
 
-const a = [1];
-console.dir(a);
+const User = function(email, password) {
+	this.email = email;
+	this.password = password + '%%%';
+}
+
+const user1 = new User('a@a.ru', '123');
+console.log(user1);
+const user2 = new User('a2@a.ru', '1234');
+console.log(user2);
+console.log(user2 instanceof User);
+
+// Создаётся пустой объект
+// Вызывается User функция
+// this = пустому объекту
+// объект связывается с prototype
+// возвращается объект
