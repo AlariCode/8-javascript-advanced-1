f82c647c03ec9e82891941adc4cedbdbe221ecfa
Alaricode <antonlarichev@gmail.com>
2023-01-13 20:33:10 +0300

    Setters и getters

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 'use strict';
 
-const Book = function(title, author) {
-	this.author = author;
-	this.title = title;
-	this.isRead = false;
-}
-Book.prototype.read = function() {
-	this.isRead = true;
-};
+const task = {
+	title: 'Task1',
+	dueTo: new Date('2023/01/01'),
 
-class BookClass {
-	isRead = false;
+	get isOverdue() {
+		return this.dueTo < new Date();
+	},
 
-	constructor(title, author) {
-		this.author = author;
-		this.title = title;
-	}
-
-	read() {
-		this.isRead = true;
+	set isOverdue(isOverdueTask) {
+		if (!isOverdueTask) {
+			this.dueTo = new Date();
+		}
 	}
 }
 
-const lotr = new BookClass('lotr', 'Tolkien');
-console.log(lotr);
-console.log(lotr instanceof BookClass);
-lotr.read();
-console.log(lotr.__proto__);
+console.log(task.isOverdue);
+task.isOverdue = false;
+console.log(task);
+
+
+class Task {
+	constructor(title, dueDate) {
+		this.title = title,
+		this.dueDate = dueDate;
+	}
+
+	get isOverdue() {
+		return this.dueTo < new Date();
+	}
+
+	set dueDate(date) {
+		if (date < new Date()) {
+			return;
+		}
+		this._dueDate = date;
+	}
+}
+
+const newTask = new Task('Task2', new Date('2023/1/1'));
+// console.log(newTask.dueDate = new Date('2024/1/1'));
+console.log(newTask._dueDate = new Date());
