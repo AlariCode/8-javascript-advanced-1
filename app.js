b3b280622495e09758730a554c36ff48a7829788
Alaricode <antonlarichev@gmail.com>
2023-01-13 19:32:12 +0300

    Цепочки prototype

--- app.js
+++ app.js
@@ -1,52 +1,13 @@
 'use strict';
 
-/*
-	Реализовать на функциях и прототипах корзину товаров с методами
-	- Добавить товар
-	- Увеличить число товаров
-	- Уменьшить число товаров (удалить если их 0)
-*/
-const product = { id: 1, name: 'Bread', count: 1 };
-
-const Cart = function() {
-	this.products = [];
+const Book = function(title, author) {
+	this.author = author;
+	this.title = title;
 }
-Cart.prototype.addProduct = function(product) {
-	if (this.products.find(product => product.id === product.id)) {
-		return;
-	}
-	this.products.push(product);
-}
+Book.prototype.isRead = false;
 
-Cart.prototype.increaseAmount = function(id) {
-	this.products = this.products.map(product => {
-		if (product.id == id) {
-			product.count++;
-			return product;
-		}
-		return product;
-	})
-}
+const lordOfTheRings = new Book('1', '1');
 
-Cart.prototype.decreaseAmount = function(id) {
-	this.products = this.products
-		.map(product => {
-			if (product.id == id) {
-				product.count--;
-				return product;
-			}
-			return product;
-		})
-		.filter(product => product.count > 0);
-}
+console.log(lordOfTheRings.hasOwnProperty('title'));
 
-const cart = new Cart();
-cart.addProduct(product);
-cart.increaseAmount(1);
-cart.decreaseAmount(1);
-cart.decreaseAmount(1);
-console.log(cart)
-
-const cart2 = new Cart();
-cart2.addProduct(product);
-console.log(cart2)
+console.log(Book.prototype.__proto__);
