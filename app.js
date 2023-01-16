b63572ab110d6254f93a72586fead9dddb852de3
Alaricode <antonlarichev@gmail.com>
2023-01-16 15:44:09 +0300

    Static

--- app.js
+++ app.js
@@ -1,43 +1,15 @@
 'use strict';
 
-const task = {
-	title: 'Task1',
-	dueTo: new Date('2023/01/01'),
+class Test {
+	static a = 1;
+	static hello() {
+		console.log('Hello');
+	}
 
-	get isOverdue() {
-		return this.dueTo < new Date();
-	},
-
-	set isOverdue(isOverdueTask) {
-		if (!isOverdueTask) {
-			this.dueTo = new Date();
-		}
+	static {
+		let b = 5;
+		this.a = 5;
 	}
 }
-
-console.log(task.isOverdue);
-task.isOverdue = false;
-console.log(task);
-
-
-class Task {
-	constructor(title, dueDate) {
-		this.title = title,
-		this.dueDate = dueDate;
-	}
-
-	get isOverdue() {
-		return this.dueTo < new Date();
-	}
-
-	set dueDate(date) {
-		if (date < new Date()) {
-			return;
-		}
-		this._dueDate = date;
-	}
-}
-
-const newTask = new Task('Task2', new Date('2023/1/1'));
-// console.log(newTask.dueDate = new Date('2024/1/1'));
-console.log(newTask._dueDate = new Date());
+Test.hello();
+console.log(Test.a);
