57b97c79f0d8898546131d6b5c60967c1145089c
Alaricode <antonlarichev@gmail.com>
2023-01-16 18:21:45 +0300

    Object.create

--- app.js
+++ app.js
@@ -1,53 +1,21 @@
 'use strict';
 
-/*
-	Реализовать класс пользователя, со свойствами
-	- логин
-	- пароль
-	Причём пароль при установке должен переворачиваться
-	и в таком виде храниться.
-	Пароль и логин после создания изменить нельзя. Так же у
-	класса добавить методы
-	- Смены пароля (передаём старый и новый пароль)
-	- Сверки пароля
-*/
-
-class User {
-	#login;
-	#_password;
-
-	constructor(login, password) {
-		this.#login = login;
-		this.#password = password;
+const User = {
+	init(email, password) {
+		this.email = email;
+		this.password = password;
+	},
+	log() {
+		console.log('Log');
 	}
+};
 
-	set #password(pass) {
-		this.#_password = pass.split('').reverse().join('');
-	}
+const user = Object.create(User);
+user.log();
+user.init('a@a.ru', '123');
+console.log(user);
 
-	get #password() {
-		return this.#_password.split('').reverse().join('');
-	}
-
-	get login() {
-		return this.#login;
-	}
-
-	checkPassword(pass) {
-		return this.#password === pass;
-	}
-
-	changePassword(oldPass, newPass) {
-		if (!this.checkPassword(oldPass)) {
-			return false;
-		}
-		this.#password = newPass;
-		return true;
-	}
-}
-
-const user = new User('a@a.ru', '123');
-console.log(user.checkPassword('234'));
-console.log(user.checkPassword('123'));
-console.log(user.changePassword('123', '234'));
-console.log(user)
+const admin = Object.create(user);
+console.log(admin)
+admin.log()
+console.log(admin.email)
