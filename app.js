e282ab17abbe8affdfca69deff3ad22183404da3
Alaricode <antonlarichev@gmail.com>
2023-01-17 18:53:14 +0300

    Наследование

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
 'use strict';
-/* Абстракция VS Инкапсуляция
-	- Название
-	- Режисёр
-	- Наш рейтинг
-	- Длительность
-	- Страна производства
-	- Актёры
-	- Трейлер
-	...
-*/
 
-class Film {
-	#name;
-	#author;
-	rating;
-	#length;
-
-	constructor(name, author, length) {
-		this.#name = name;
-		this.#author = author;
-		this.#length = length;
-	}
-
-	get name() {
-		return this.#name;
-	}
-
-	get author() {
-		return this.#author;
-	}
-
-	get length() {
-		return this.#length;
-	}
+const Book = function(title, author) {
+	this.title = title;
+	this.author = author;
 }
 
-const film = new Film('Avatar', 'Cameron', 240)
-console.log(film);
+Book.prototype.buy = function() {
+	console.log('Buy');
+}
+
+const AudioBook = function(title, author, lenMin) {
+	Book.call(this, title, author);
+	this.lenMin = lenMin;
+}
+
+AudioBook.prototype = Object.create(Book.prototype);
+AudioBook.prototype.constructor = AudioBook;
+AudioBook.prototype.log = function() {
+	console.log(`${this.title} - ${this.lenMin}`);
+}
+
+const book = new AudioBook('Lord Of The Rings', 'Tolkien', 20 * 60);
+book.log();
+book.buy();
+console.log(book);
+
+console.log(book instanceof AudioBook);
+console.log(book instanceof Book);
