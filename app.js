959d599ef37138c5df801d36310fbdf656c65f4f
Alaricode <antonlarichev@gmail.com>
2023-01-17 19:31:21 +0300

    Наследование в ES6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Book = function(title, author) {
 	this.title = title;
 	this.author = author;
@@ -18,12 +18,30 @@ AudioBook.prototype = Object.create(Book.prototype);
 AudioBook.prototype.constructor = AudioBook;
 AudioBook.prototype.log = function() {
 	console.log(`${this.title} - ${this.lenMin}`);
+}*/
+
+class Book {
+	constructor(title, author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	buy() {
+		console.log('Buy');
+	}
 }
 
-const book = new AudioBook('Lord Of The Rings', 'Tolkien', 20 * 60);
+class AudioBook extends Book {
+	constructor(title, author, lenMin) {
+		super(title, author);
+		this.lenMin = lenMin;
+	}
+
+	log() {
+		console.log(`${this.title} - ${this.lenMin}`);
+	}
+}
+
+const book = new AudioBook('Lord Of The Rings', 'Tolkien', 60*20);
 book.log();
 book.buy();
-console.log(book);
-
-console.log(book instanceof AudioBook);
-console.log(book instanceof Book);
