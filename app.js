3db3db169c9ed9b19a4d7426f3cef10994ff72db
Alaricode <antonlarichev@gmail.com>
2023-01-21 13:52:39 +0300

    Проблемы callback

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 'use strict';
 
-/*
-	Получить среднюю цены 30 товаров из API
-	https://dummyjson.com/products
-*/
-
 const request = new XMLHttpRequest();
 request.open('GET', 'https://dummyjson.com/products');
 request.send();
 
 request.addEventListener('load', function() {
 	const { products } = JSON.parse(this.responseText);
-	const sum = products.reduce((acc, p) => acc += p.price, 0);
-	console.log(sum / products.length);
-})
+	console.log(products);
+
+	const request = new XMLHttpRequest();
+	request.open('GET', 'https://dummyjson.com/products/' + products[0].id);
+	request.send();
+
+	request.addEventListener('load', function() {
+		const data = JSON.parse(this.responseText);
+		console.log(data);
+		
+		const request = new XMLHttpRequest();
+		request.open('GET', 'https://dummyjson.com/products/' + products[1].id);
+		request.send();
+
+		request.addEventListener('load', function() {
+			const data = JSON.parse(this.responseText);
+			console.log(data);
+		});
+	});
+});
