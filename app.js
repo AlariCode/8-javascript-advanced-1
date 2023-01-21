3c40a8a6cc84974ec60854af0706c10d810bb30d
Alaricode <antonlarichev@gmail.com>
2023-01-21 13:13:42 +0300

    Упражнение - Получение каждого продукта

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 'use strict';
 
-function req(id) {
-	const request = new XMLHttpRequest();
-	request.open('GET', 'https://dummyjson.com/products/' + id);
-	request.send();
+/*
+	Получить среднюю цены 30 товаров из API
+	https://dummyjson.com/products
+*/
 
-	request.addEventListener('load', function() {
-		const data = JSON.parse(this.responseText);
-		console.log(data);
-	});
-}
+const request = new XMLHttpRequest();
+request.open('GET', 'https://dummyjson.com/products');
+request.send();
 
-req(1);
-req('');
-req(3);
-
-
-console.log('end');
+request.addEventListener('load', function() {
+	const { products } = JSON.parse(this.responseText);
+	const sum = products.reduce((acc, p) => acc += p.price, 0);
+	console.log(sum / products.length);
+})
