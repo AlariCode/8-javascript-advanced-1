7bc94d526ea48df68eb734a335b52695c1a110d3
Alaricode <antonlarichev@gmail.com>
2023-01-24 20:40:21 +0300

    Обработка promises

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
 
-const res = fetch('https://dummyjson.com/products/1');
-console.log(res);
+fetch('https://dummyjson.com/products/1')
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+	});
