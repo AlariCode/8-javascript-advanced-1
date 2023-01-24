a86a26336c21681c0c38c5e1698a669817df3d19
Alaricode <antonlarichev@gmail.com>
2023-01-24 19:47:02 +0300

    Promises и fetch

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
 'use strict';
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://dummyjson.com/products');
-request.send();
-
-request.addEventListener('load', function() {
-	const { products } = JSON.parse(this.responseText);
-	console.log(products);
-
-	const request = new XMLHttpRequest();
-	request.open('GET', 'https://dummyjson.com/products/' + products[0].id);
-	request.send();
-
-	request.addEventListener('load', function() {
-		const data = JSON.parse(this.responseText);
-		console.log(data);
-		
-		const request = new XMLHttpRequest();
-		request.open('GET', 'https://dummyjson.com/products/' + products[1].id);
-		request.send();
-
-		request.addEventListener('load', function() {
-			const data = JSON.parse(this.responseText);
-			console.log(data);
-		});
-	});
-});
+const res = fetch('https://dummyjson.com/products/1');
+console.log(res);
