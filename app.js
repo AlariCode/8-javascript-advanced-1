877256efb4a9a658bc1cc5152d6ae09bdfc24bf2
Alaricode <antonlarichev@gmail.com>
2023-01-26 18:49:19 +0300

    Упражнение - Мой город

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 'use strict';
 
-async function getProducts() {
+/*
+	Получить геолокацию пользователя через
+	Geolocation.getCurrentPosition() (WEB API)
+	и по координатам определить город, отправив запрос:
+	https://api.bigdatacloud.net/data/reverse-geocode-client?
+	latitude=00&longitude=00
+*/
+
+function getMyCoordinates() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(
+			({ coords }) => {
+				resolve({ 
+					latitude: coords.latitude,
+					longitude: coords.longitude,
+				});
+			},
+			(err) => {
+				reject(err);
+			}
+		);
+	});
+}
+
+async function getMyCity() {
 	try {
-		const productsResponse = await fetch('https://dummyjson.com/products');
-		if (!productsResponse.ok) {
-			throw new Error(productsResponse.status);
+		const { latitude, longitude } = await getMyCoordinates();
+		const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`);
+		if (!response.ok) {
+			throw new Error(response.status);
 		}
-		const { products } = await productsResponse.json();
-		console.log(products);
-		
-		const productResponse = await fetch('https://dummyjson.com/products/' + products[0].id);
-		const product = await productResponse.json();
-		console.log(product);
+		const data = await response.json();
+		console.log(data.city);
 	} catch(e) {
 		console.error(e);
-	} finally {
-		console.log('Finally')
 	}
 }
 
-try {
-	JSON.parse('{d');
-} catch(e) {
-	console.error('sdsf')
-}
-
-getProducts();
-console.log('End');
+getMyCity()
