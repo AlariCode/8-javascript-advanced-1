70328e29a872e9001dd262f307108769c7f7d74a
Alaricode <antonlarichev@gmail.com>
2023-01-26 15:22:56 +0300

    Упражнение - создание fetch

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 'use strict';
 
-const prom = new Promise((resolve) => {
-	console.log('Constuctor');
-	// for (let i = 0; i< 10000000000; i++) {}
-	setTimeout(() => {
-		resolve('Timer');
-	}, 1000);
-})
-prom.then(data => console.log(data));
+/*
+	Сделать функцию myFetch, которая выполняет внутри
+	XMLHttpRequest
+*/
 
-Promise.reject(new Error('Error')).catch(error => console.error(error));
-Promise.resolve('Instant').then(data => console.log(data));
+function myFetch(url) {
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest();
+		request.open('GET', url);
+		request.send();
+
+		request.addEventListener('load', function() {
+			if(this.status > 400) {
+				reject(new Error(this.status));
+			};
+			resolve(this.responseText);
+		});
+
+		request.addEventListener('error', function() {
+			reject(new Error(this.status));
+		})
+		
+		request.addEventListener('timeout', function() {
+			reject(new Error('Timeout'));
+		})
+	})
+}
+
+myFetch('https://dummyjson.com/productss')
+	.then(data => console.log(data))
+	.catch(err => console.error(err))
