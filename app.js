64a2158336971a0ff9988d9a08739d95993d9de9
Alaricode <antonlarichev@gmail.com>
2023-01-28 13:15:25 +0300

    Параллельное выполнение

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 'use strict';
 
-const asyncArrow = async () => {
-	try {
-		const response = await fetch('https://dummyjson.com/products');
-		const data = await response.json();
-		return data
-	} catch(e) {
-		console.error(e);
-		throw e;
-	}
+async function gatAllProducts() {
+	const response = await fetch('https://dummyjson.com/products');
+	return response.json();
 }
 
-// console.log('1');
-// asyncArrow()
-// 	.then(data => {
-// 		console.log(data)
-// 	})
-// 	.catch(e => console.error(e))
-// 	.finally(() => console.log('2'));
+async function getProduct(id) {
+	const response = await fetch('https://dummyjson.com/products/' + id);
+	return response.json();
+}
 
-(async () => {
-	console.log('1');
-	const res = await asyncArrow();
-	console.log(res);
-	console.log('2')
-})();
+async function getProductError(id) {
+	const response = await fetch('https://dummyjsons.com/products/' + id);
+	return response.json();
+}
+
+async function main() {
+	const { products } = await gatAllProducts();
+	const res = await Promise.all(products.map(product => getProduct(product.id)));
+	console.log(res)
+	// for (const product of products) {
+	// 	const res = await getProduct(product.id);
+	// 	console.log(res);
+	// }
+}
+
+main();
