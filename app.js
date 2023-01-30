7735949b7f9b895bfeb2c2adc7ef24f2f06f426b
Alaricode <antonlarichev@gmail.com>
2023-01-30 20:36:24 +0300

    Все возможности fetch

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 'use strict';
 
-async function getProduct(id) {
-	const response = await fetch('https://dummyjson.com/products/' + id);
-	return response.json();
-}
-
-async function getProductError(id) {
-	const response = await fetch('https://dummyjsons.com/products/' + id);
-	return response.json();
-}
-
 async function main() {
-	const res1 = await Promise.all([
-		getProduct(1),
-		getProduct(2),
-	]);
-	console.log(res1);
-	const res2 = await Promise.allSettled([
-		getProduct(1),
-		getProductError(2),
-	]);
-	console.log(res2);
-	const res3 = await Promise.race([
-		getProduct(1),
-		getProductError(2),
-	]);
-	console.log(res3);
+	const res = await fetch('https://dummyjson.com/auth/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			username: 'kminchelle',
+			password: '0lelplR'
+		})
+	});
+	const data = await res.json();
+	console.log(data);
 }
 
 main();
