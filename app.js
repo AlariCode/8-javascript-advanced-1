6b8ffac441c8e1fd12b4d7f188c02a3a26f84a92
Alaricode <antonlarichev@gmail.com>
2023-02-01 21:43:31 +0300

    Упражнение - Генератор активностей

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 'use strict';
 
-async function main() {
-	const res = await fetch('https://dummyjson.com/auth/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			username: 'kminchelle',
-			password: '0lelplR'
-		})
-	});
-	const data = await res.json();
-	console.log(data);
+/*
+	Сделать генератор 3х идей от скуки
+	https://www.boredapi.com/api/activity
+	с отображением на странице
+*/
+const wrapper = document.querySelector('.wrapper');
+
+async function getActivity() {
+	const res = await fetch('https://www.boredapi.com/api/activity');
+	return res.json();
 }
 
-main();
+async function generate() {
+	try {
+		wrapper.innerHTML = '';
+		const data = await Promise.all([
+			getActivity(),
+			getActivity(),
+			getActivity(),
+		]);
+		console.log(data);
+		for (const el of data) {
+			const element = document.createElement('div');
+			element.innerHTML = `${el.activity}`;
+			wrapper.appendChild(element);
+		}
+	} catch(e) {
+		console.error(e);
+	}
+}
