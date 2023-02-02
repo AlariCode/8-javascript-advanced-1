6c1a35c7c9d311dfa6643dac25ba4356fe00da6b
Alaricode <antonlarichev@gmail.com>
2023-02-02 21:02:40 +0300

    Работа с элементами

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
 'use strict';
 
-/*
-	Сделать генератор 3х идей от скуки
-	https://www.boredapi.com/api/activity
-	с отображением на странице
-*/
-const wrapper = document.querySelector('.wrapper');
+console.log(document.head);
+console.log(document.body);
 
-async function getActivity() {
-	const res = await fetch('https://www.boredapi.com/api/activity');
-	return res.json();
-}
+const el = document.querySelector('.wrapper');
+const el2 = document.querySelectorAll('meta');
+console.log(el);
+console.log(el2);
+const el3 = document.getElementsByClassName('wrapper');
+const el4 = document.getElementsByTagName('meta');
+console.log(el3);
+console.log(el4);
 
-async function generate() {
-	try {
-		wrapper.innerHTML = '';
-		const data = await Promise.all([
-			getActivity(),
-			getActivity(),
-			getActivity(),
-		]);
-		console.log(data);
-		for (const el of data) {
-			const element = document.createElement('div');
-			element.innerHTML = `${el.activity}`;
-			wrapper.appendChild(element);
-		}
-	} catch(e) {
-		console.error(e);
-	}
+const button = document.createElement('button');
+button.innerHTML = 'тест';
+
+const button2 = document.createElement('button');
+button2.innerHTML = 'тест2';
+
+el.append(button);
+// el.prepend(button2);
+// el.before(button2);
+el.after(button2);
+
+function generate() {
+	console.log(el.parentNode);
+	el.remove();
 }
