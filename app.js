1b681fb021f0d1587e68592d930e0ffa982b56f6
Alaricode <antonlarichev@gmail.com>
2023-02-03 16:20:38 +0300

    Визуальное положение элементов

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 'use strict';
 
-console.log(document.head);
-console.log(document.body);
+function generate(event) {
+	console.log(event.target.getBoundingClientRect());
 
-const el = document.querySelector('.wrapper');
-const el2 = document.querySelectorAll('meta');
-console.log(el);
-console.log(el2);
-const el3 = document.getElementsByClassName('wrapper');
-const el4 = document.getElementsByTagName('meta');
-console.log(el3);
-console.log(el4);
+	console.log(`X offset: ${window.pageXOffset}`);
+	console.log(`Y offset: ${window.pageYOffset}`);
+	console.log(`clientWidth: ${document.documentElement.clientWidth}`);
+	console.log(`clientHeight: ${document.documentElement.clientHeight}`);
 
-const button = document.createElement('button');
-button.innerHTML = 'тест';
+	const el = document.querySelector('.down');
+	const rect = el.getBoundingClientRect();
 
-const button2 = document.createElement('button');
-button2.innerHTML = 'тест2';
-
-el.append(button);
-// el.prepend(button2);
-// el.before(button2);
-el.after(button2);
-
-function generate() {
-	console.log(el.parentNode);
-	el.remove();
+	window.scrollTo({
+		left: window.pageXOffset + rect.left,
+		top: window.pageYOffset + rect.top,
+		behavior: 'smooth'
+	});
 }
