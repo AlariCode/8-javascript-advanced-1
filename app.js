fad97f9a2c496f142505f0a0ea0bffaa2f406956
Alaricode <antonlarichev@gmail.com>
2023-02-03 19:30:24 +0300

    Пример всплытия событий

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 'use strict';
 
 const button = document.querySelector('.button');
+const inner = document.querySelector('.inner');
+const wrapper = document.querySelector('.wrapper');
 
-const eventHandler = function (event) {
-	console.log('Event 1');
-}
+button.addEventListener('click', function (event) {
+	console.log('button');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'purple';
+});
 
-button.addEventListener('mouseover', eventHandler);
-button.addEventListener('click', (event) => {
-	console.log('Event 2');
+inner.addEventListener('click', function (event) {
+	console.log('inner');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'blue';
+	// event.stopPropagation();
+});
+
+wrapper.addEventListener('click', function (event) {
+	console.log('wrapper');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'green';
+}, true);
+
+wrapper.addEventListener('click', function (event) {
+	console.log('wrapper');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'green';
 });
