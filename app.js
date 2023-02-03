fe23a719ade87946b71cb832a06b978593f4d34e
Alaricode <antonlarichev@gmail.com>
2023-02-03 18:20:06 +0300

    Типы событий и обработчики

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 'use strict';
 
-function generate(event) {
-	console.log(event.target.getBoundingClientRect());
+const button = document.querySelector('.button');
 
-	console.log(`X offset: ${window.pageXOffset}`);
-	console.log(`Y offset: ${window.pageYOffset}`);
-	console.log(`clientWidth: ${document.documentElement.clientWidth}`);
-	console.log(`clientHeight: ${document.documentElement.clientHeight}`);
-
-	const el = document.querySelector('.down');
-	const rect = el.getBoundingClientRect();
-
-	window.scrollTo({
-		left: window.pageXOffset + rect.left,
-		top: window.pageYOffset + rect.top,
-		behavior: 'smooth'
-	});
+const eventHandler = function (event) {
+	console.log('Event 1');
 }
+
+button.addEventListener('mouseover', eventHandler);
+button.addEventListener('click', (event) => {
+	console.log('Event 2');
+	button.removeEventListener('click', eventHandler);
+});
