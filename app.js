5919f44454ea66becfa24b89be86ce6923cbae5c
Alaricode <antonlarichev@gmail.com>
2023-02-05 11:27:52 +0300

    Упражнение - Поиск по в списку

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 'use strict';
+/* 
+	Динамически создать N элементов с текстом и поле
+	для поиска. При вводе в поле, выделять элементы,
+	которые содержат введённый текст
+*/
 
-document.addEventListener('DOMContentLoaded', function(e) {
-	console.log('DOMContentLoaded');
-	console.log(e);
-});
+const wrapper = document.querySelector('.wrapper');
+for (let i = 0; i < 100; i++) {
+	const el = document.createElement('div');
+	el.innerHTML = i;
+	wrapper.append(el);
+}
 
-window.addEventListener('load', function(e) {
-	console.log('load');
-	console.log(e);
-});
-
-// window.addEventListener('beforeunload', function(e) {
-// 	e.preventDefault();
-// 	e.returnValue = '';
-// });
+function search(event) {
+	const inputValue = event.target.value;
+	for (const el of [...wrapper.children]) {
+		if (el.innerHTML.includes(inputValue)) {
+			el.classList.add('yellow');
+			continue;
+		}
+		el.classList.remove('yellow');
+	}
+}
