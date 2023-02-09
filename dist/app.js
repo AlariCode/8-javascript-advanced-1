a8beff57154a1f09727138215cc46c9399d06ae3
Alaricode <antonlarichev@gmail.com>
2023-02-09 18:01:22 +0300

    Вёрстка шапки

--- dist/app.js
+++ dist/app.js
@@ -1039,6 +1039,19 @@
 			<div>
 				<img src="/static/logo.svg" alt="Логотип" />
 			</div>
+			<div class="menu">
+				<a class="menu__item" href="#">
+					<img src="/static/search.svg" alt="Поиск иконка" />
+					Поиск книг
+				</a>
+				<a class="menu__item" href="#favorites">
+					<img src="/static/favorites.svg" alt="Избранное иконка" />
+					Избранное
+					<div class="menu__counter">
+						${this.appState.favorites.length}
+					</div>
+				</a>
+			</div>
 		`;
 			return this.el;
 		}
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from '../../common/div-component';
+import './header.css';
 
 export class Header extends DivComponent {
 	constructor(appState) {
@@ -12,6 +13,19 @@ export class Header extends DivComponent {
 		this.el.innerHTML = `
 			<div>
 				<img src="/static/logo.svg" alt="Логотип" />
+			</div>
+			<div class="menu">
+				<a class="menu__item" href="#">
+					<img src="/static/search.svg" alt="Поиск иконка" />
+					Поиск книг
+				</a>
+				<a class="menu__item" href="#favorites">
+					<img src="/static/favorites.svg" alt="Избранное иконка" />
+					Избранное
+					<div class="menu__counter">
+						${this.appState.favorites.length}
+					</div>
+				</a>
 			</div>
 		`;
 		return this.el;
